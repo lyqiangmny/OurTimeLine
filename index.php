4bfc61bbce782c20e5e4edca142a6ec23c2af120
lyqiangmny <546604727@qq.com>
2016-05-21 16:27:19 +0800

    修改音乐
    修改播放音乐为多首循环，不再引用外部链接。

--- index.php
+++ index.php
@@ -106,8 +106,11 @@
     </div>
     
   </div>
-   <div align="center" style="padding-top:40px;">
-    <audio src="http://sc.111ttt.com/up/mp3/242273/1B22FF6ECB5DCA0484760D440ACBF73A.mp3" controls="controls" autoplay="true">您的浏览器不支持背景音乐播放。</audio>
+    <div align="center" style="padding-top:40px;">
+    <audio controls="controls" loop="loop" autoplay="true">
+     <source src="midian.mp3" type="audio/mpeg" />
+     <source src="dangni.mp3" type="audio/mpeg" />
+     您的浏览器不支持背景音乐播放。</audio>
     </div>
     <div align="center" style="padding-top:10px;">Copyright © 2015-2016  All Rights Reserved By Mr.C00der.Gu0 </div>
     <div class="bshare-custom" align="center" style="padding-top:10px;"><div class="bsPromo bsPromo2"></div><a title="分享到QQ空间" class="bshare-qzone"></a><a title="分享到QQ好友" class="bshare-qqim" href="javascript:void(0);"></a><a title="分享到新浪微博" class="bshare-sinaminiblog"></a><a title="分享到微信" class="bshare-weixin" href="javascript:void(0);"></a><a title="分享到i贴吧" class="bshare-itieba" href="javascript:void(0);"></a><a title="分享到人人网" class="bshare-renren"></a><a title="分享到腾讯微博" class="bshare-qqmb"></a><a title="分享到复制网址" class="bshare-clipboard" href="javascript:void(0);"></a><a title="更多平台" class="bshare-more bshare-more-icon more-style-addthis"></a></div><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/buttonLite.js#style=-1&amp;uuid=&amp;pophcol=1&amp;lang=zh"></script><script type="text/javascript" charset="utf-8" src="http://static.bshare.cn/b/bshareC0.js"></script>
